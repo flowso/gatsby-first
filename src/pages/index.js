d3b3b8a60d2b677529694c0f0342b2465e171f2f
Vy <amazing.diamondude@gmail.com>
2022-05-02 17:14:23 +0200

    done with the image part

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import Layout from '../component/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () =>
 {
   return (
     <Layout pageTitle="Home Page">
       <p>this is the base</p>
+      <StaticImage alt="ecchi" src="../images/goose.jpg" />
     </Layout>
   )
 }
